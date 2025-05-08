6c06c04d3936d3a3b8349994ecadfd3454aa0f90
Kushan2k <kushangayantha001@gmail.com>
2025-05-08 12:37:08 +0530

    chat routes added

--- routers/user_router.py
+++ routers/user_router.py
@@ -1,11 +1,24 @@
 from fastapi import APIRouter
+from sklearn.utils import _joblib as jb
+from sklearn.preprocessing import StandardScaler
 
+print("Loading user router...")
 
 router=APIRouter(
-    prefix="/user",
-    tags=["user"],
-    responses={404: {"description": "Not found"}},
+    prefix="/api/v1/user",
+    
 )
+
+@router.get("/test")
+async def chat():
+    """
+    Chat with the user.
+    """
+
+    msgs={
+        1:'3434'
+    }
+    return {"message": msgs}
 
 
 @router.get("/")
