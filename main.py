ff9a7eb806ad89e6918a8f99b442f9f6d57febcf
Kushan2k <kushangayantha001@gmail.com>
2025-05-21 16:53:47 +0530

    index route added

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ app = FastAPI(
     version="1.0.0",
 )
 
+@app.get('/')
+async def index():
+    """
+    Root endpoint.
+    """
+    return {"message": "Welcome to the Chat API!"}
+
 # routes definitions 
 app.include_router(chat_route.router)
 app.include_router(user_router.router)
